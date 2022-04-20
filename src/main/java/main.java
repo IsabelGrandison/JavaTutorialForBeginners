035928691152b4e8b84ff461688d9ffc8ed7abde
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 09:54:15 -0400

    Constants

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
+
 
 public class main {
     public static void main(String[] args) {
-    int [][] numbers = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(Arrays.deepToString(numbers));
+    final float PI = 3.14F;
 
 
 
