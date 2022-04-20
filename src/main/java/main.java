4a38786a8187732ee0e8e3b2604af1a889c9cc01
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 16:51:11 -0400

    Break and Continue

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
 public class main {
         public static void main(String[] args) {
-              Scanner scanner = new Scanner (System.in);
-              String input = "";
-              while (!input .equals("quit")) {
-                      System.out.println("Input: ");
-                      input = scanner.next().toLowerCase();
-                      System.out.println(input);
-              }
-
-
-
-
-        }
-
-}
+                Scanner scanner = new Scanner(System.in);
+                String input = "";
+                while (!input.equals("quit")) {
+                        System.out.println("Input: ");
+                        input = scanner.next().toLowerCase();
+                        if (!input.equals("quit"))
+                                System.out.println(input);
+                }
+        }}
