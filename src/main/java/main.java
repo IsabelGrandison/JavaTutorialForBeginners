9669ee84a175c85538f970b626a2d7254fb57406
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 16:00:24 -0400

    The Ternary Operator

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,9 @@
 
 public class main {
         public static void main(String[] args) {
-        int temp = 32;
-        if (temp > 30);
-            System.out.println("It's a hot day");
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "economy";
+
 
 
 
