a3f1d89954ac46087977ab2c6b752b0bbcf4676a
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-19 23:42:14 -0400

    Reference Types

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,10 @@
+import java.util.Date;
 public class main {
     public static void main(String[] args) {
-        byte age = 26;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
+        byte age = 30;
+        Date now = new Date ();
+        System.out.println(now);
+
 
 
     }
