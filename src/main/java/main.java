782643e3742aa15f845d2c72402b9a77ca09ca2b
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-19 23:24:10 -0400

    Variables

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int myAge = 26;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
