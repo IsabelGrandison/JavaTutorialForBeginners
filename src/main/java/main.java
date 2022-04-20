5d834fa6f0e9beb80e2043a9207aa39ea0b3f7e3
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 00:15:34 -0400

    Strings

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,8 @@
-import java.awt.*;
+
 public class main {
     public static void main(String[] args) {
-      Point point1 = new Point (1, 1);
-      Point point2 = point1;
-      point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+        System.out.println(message.replace("!", "*"));
 
 
 
