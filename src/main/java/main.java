673c1028ae8dd40fedec1a327b237dbe4afd5d53
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 15:09:01 -0400

    Logical Operators

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,9 @@
 
 public class main {
         public static void main(String[] args) {
-                int x = 1;
-                int y = 2;
-                System.out.println(x <= y);
-
+          int temperature = 12;
+          boolean isWarm = temperature > 20 && temperature < 30;
+            System.out.println(isWarm);
 
 
 
