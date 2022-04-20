f48087e621ad301f1c4d10b04f4f69b098e85970
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-20 10:08:42 -0400

    Arithmetic Expressions

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,12 @@
-
-
 public class main {
-    public static void main(String[] args) {
-    final float PI = 3.14F;
+        public static void main(String[] args) {
+                int x = 1;
+                int y = x++;
+                System.out.println(x);
+                System.out.println(y);
+
+        }
 
 
 
-    }
 }
