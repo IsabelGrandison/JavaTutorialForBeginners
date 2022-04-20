36c5c1ff499d3a2ae8e936155366db887e191fc0
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-19 23:33:42 -0400

    Primitive Types

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,11 @@
 public class main {
     public static void main(String[] args) {
-        int myAge = 26;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 26;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+
     }
 }
